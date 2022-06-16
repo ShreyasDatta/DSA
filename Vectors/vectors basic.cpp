7a2d57e5bd776895015a5daaa6b069806480bf7d
Siddhant Priyadarshi <siddhantp4ever@gmail.com>
2022-06-16 19:18:52 +0530

    Update vectors basic.cpp

--- Vectors/vectors basic.cpp
+++ Vectors/vectors basic.cpp
@@ -4,29 +4,38 @@ using namespace std;
 
 int main(){
 
-    vector<int> v;                            //Initializig vector
+    vector<int> v;                                          //Initializig vector
     vector<int> v2 (3, 50;)
 
-    v.push_back(1);                           //Pushing values to vector
+    v.push_back(1);                                         //Pushing values to vector
     v.push_back(2);
     v.push_back(3);
+    
+    v.pop_back();                                           //Pop element at back
+    
+    v.size();                                               //Size of Vector
 
-    for(int i=0; i<v.size(); i++){              //Traversing Vector
+    for(int i=0; i<v.size(); i++){                          //Traversing Vector 1
         cout<<v[i]<<endl;
     }
 
     vector<int>::iterator it;
-    for(it=v.begin();it!=v.end(); it++){      //Traversing Vector
+    for(it=v.begin();it!=v.end(); it++){                    //Traversing Vector 2
         cout<<*it<<endl;
     }
 
-    for(auto element:v){                      //Traversing Vector
+    for(auto element:v){                                    //Traversing Vector 3
         cout<<element<<endl;
     }
+    
+    reverse(v.begin(), v.end());                            //reverses the vector 
 
-    v.pop_back();                             //Pop element at back
+    swap(v,v2);                                             //Swap elements of both vector
 
-    swap(v,v2);                               //Swap elements of both vector
-
-    sort(v.begin(), v.end());
+    sort(v.begin(), v.end());                               //Sorting in Ascending Order
+    
+    sort(v.begin(), v.end(), std::greater<int> ());          // Sorting in Descending Order
+    
+    return 0;
+    
 }
